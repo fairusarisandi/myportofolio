be35b19867de14d1a3274b4182719f36d1984e73
Fairus Arisandi <fairuzarisandi06@gmail.com>
2026-09-16 21:38:57 +0700

    feat(tutorial 03): add form for project page

---
+++ main/forms.py
@@ -0,0 +1,38 @@
+from django.forms import ModelForm, TextInput, Textarea, URLInput
+
+from main.models import Project
+
+class ProjectForm(ModelForm):
+    class Meta:
+        model = Project
+        fields = [
+            "name",
+            "description",
+            "thumbnail",
+        ]
+
+        labels = {
+            "name": "Nama Proyek",
+            "description": "Deskripsi Proyek",
+            "thumbnail": "Thumbnail Proyek",
+        }
+
+        widgets = {
+            "name": TextInput(
+                attrs={
+                    "placeholder": "Portfolio Website",
+                    "maxlength": 255,
+                }
+            ),
+            "description": Textarea(
+                attrs={
+                    "placeholder": "Deskripsi Proyek",
+                    "rows": 3,
+                }
+            ),
+            "thumbnail": URLInput(
+                attrs={
+                    "placeholder": "https://drive.google.com/thumbnail?id=...&sz=w1000",
+                }
+            ),
+        }
--- portofolio/settings.py
+++ portofolio/settings.py
@@ -75,6 +75,8 @@ WHITENOISE_USE_FINDERS = True
 
 WSGI_APPLICATION = 'portofolio.wsgi.application'
 
+CSRF_TRUSTED_ORIGINS = ["https://muhammad-fairus51-myportofolio.pws.cs.ui.ac.id"]
+
 
 # Database
 # https://docs.djangoproject.com/en/6.1/ref/settings/#databases
